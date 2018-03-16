651c7741aaa71c9da5cb54660f2498bed180451b
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-16 17:57:58 +0000

    started work on scraping with books.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require "pry"
 require 'nokogiri'
 require 'open_uri_redirections'
 
-require_relative "../lib/book_scraping/version"
-require_relative "../lib/book_scraping/cli"
-require_relative "../lib/book_scraping/books"
-require_relative "../lib/book_scraping/subjects"
+require_relative "../lib/book_scraper/version"
+require_relative "../lib/book_scraper/cli"
+require_relative "../lib/book_scraper/books"
+require_relative "../lib/book_scraper/subjects"
---
+++ lib/book_scraper/books.rb
@@ -0,0 +1,28 @@
+class BookScraper::Books
+
+  attr_accessor :book_info, :books
+
+  @@all = []
+
+  def initialize
+    scrape_books
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def gets_page
+    doc = Nokogiri::HTML(open('http://books.toscrape.com/catalogue/category/books_1/page-1.html'))
+  end
+
+  def scrape_books
+    @books = gets_page.css("ol.row h3 a").collect {|book|book.values}
+  end
+
+  def book_titles
+    @books.detect.with_index {|book, index| "#{index+1}. #{book}[1]"}
+  end
+
+end
