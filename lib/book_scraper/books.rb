f5081c473215c6d79da532c86c049f9f1c6809da
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-22 00:20:49 +0000

    Altered methods to allow CLI to alter and display information I needed

--- lib/book_scraper/books.rb
+++ lib/book_scraper/books.rb
@@ -1,32 +1,42 @@
 class BookScraper::Books
 
-  attr_accessor :book_page, :books, :subject_page
+  attr_accessor :book_page, :books, :subject_page, :sub_name
 
-  def initialize(sub)
-    @subject_page = sub
+  def initialize(sub_page, sub)
+    @subject_page = sub_page
+    @sub_name = sub
+    @books = []
+    @book_page = []
     scrape_books
-    book_page
   end
 
   def get_page
     Nokogiri::HTML(open(@subject_page))
   end
 
-  def self.all
-    @@all
+  def scrape_books
+    @books << get_page.css("ol.row h3 a").collect {|book|book.values.last}
+    if get_page.css("li.next").any?
+      page = get_page.css("li.next a").first.values.first
+      next_page = @subject_page[0..-11] + page
+      scrape_next_page(next_page)
+    else
+      @books = @books.join("+").split("+")
+    end
   end
 
-  def scrape_books
-    @books = get_page.css("ol.row h3 a").collect {|book|book.values.last}
+  def scrape_next_page(next_page)
+    @subject_page = next_page
+    scrape_books
+    book_page
   end
 
   def book_page
-    @book_page = get_page.css("ol.row h3 a").collect {|book|"http://books.toscrape.com/catalogue/#{book.values.first[9..-1]}"}
+    @book_page << get_page.css("ol.row h3 a").collect {|book|"http://books.toscrape.com/catalogue/#{book.values.first[9..-1]}"}
   end
 
-  def titles
-    @books.detect.with_index {|book, index| puts "#{index+1}. #{book}"}
+  def books
+    @books
   end
-
 
 end
--- lib/book_scraper/subjects.rb
+++ lib/book_scraper/subjects.rb
@@ -3,7 +3,7 @@ class BookScraper::Subjects
   attr_accessor :subjects, :subject_page
 
   def initialize
-    subjects
+    scrape_subjects
     subject_page
   end
 
@@ -11,12 +11,16 @@ class BookScraper::Subjects
     Nokogiri::HTML(open('http://books.toscrape.com/index.html'))
   end
 
-  def subjects
+  def scrape_subjects
     @subjects = gets_page.css("div.side_categories li ul a").map {|subject|subject.text.strip}
   end
 
   def subject_page
     @subject_page = gets_page.css("div.side_categories li ul a").map {|subject|"http://books.toscrape.com/#{subject.values.first}"}
+  end
+
+  def list_subjects
+    @subjects.detect.with_index {|subject, index| puts "#{index+1}. #{subject}"}
   end
 
 end
